884c1001268c8d9b0613acdc6212c23c38f0d719
AlicFeng <a@samego.com>
2020-11-27 14:09:49 +0800

    update:ingress:list

--- src/Kubernetes/Kubernetes/Ingress.php
+++ src/Kubernetes/Kubernetes/Ingress.php
@@ -16,9 +16,7 @@ class Ingress extends KubernetesClient implements KubernetesManagerIf
     /**
      * @function    新建资源项
      * @description 新建资源项
-     *
      * @param array $package 期待的资源配置
-     *
      * @return $this
      */
     public function create(array $package = [])
@@ -31,10 +29,8 @@ class Ingress extends KubernetesClient implements KubernetesManagerIf
     /**
      * @function    修改资源项
      * @description 修改指定资源项
-     *
      * @param string $name    资源名称
      * @param array  $package 期待的资源配置
-     *
      * @return $this
      */
     public function apply(string $name, array $package = [])
@@ -59,23 +55,24 @@ class Ingress extends KubernetesClient implements KubernetesManagerIf
     /**
      * @function    查询资源列表结合
      * @description 默认为具体的默认命名空间
-     *
      * @param bool  $is_all_namespace 是否为所有命名空间
      * @param array $query_parameters 列表查询参数，详见：https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.17
-     *
      * @return $this
      */
     public function list(bool $is_all_namespace = false, array $query_parameters = [])
     {
-        // TODO: Implement list() method.
+        $uri = "/apis/extensions/v1beta1/namespaces/{$this->namespace}/ingresses";
+        if ($is_all_namespace) {
+            $uri = '/apis/extensions/v1beta1/ingresses';
+        }
+
+        return $this->_list($uri, $query_parameters);
     }
 
     /**
      * @function    查询资源项状态
      * @description 查询资源项状态
-     *
      * @param string $name 资源项名称
-     *
      * @return $this
      */
     public function queryStatus(string $name)
@@ -86,14 +83,14 @@ class Ingress extends KubernetesClient implements KubernetesManagerIf
     /**
      * @function    修改部分资源项
      * @description 修改部分资源项
-     *
      * @param string $name    资源项名称
      * @param array  $package 期待的资源配置
-     *
      * @return $this
      */
     public function repair(string $name, array $package)
     {
-        // TODO: Implement repair() method.
+        $uri = "/apis/extensions/v1beta1/namespaces/{$this->namespace}/ingresses/{$name}";
+
+        return $this->_repair($uri, self::TYPE_INGRESS, $package);
     }
 }
